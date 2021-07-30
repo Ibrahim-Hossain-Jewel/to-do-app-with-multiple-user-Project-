a2c23a1f659df519359daccc3eedf1c515381e2a
ibrahim hossain <ibrahimhossaincse@gmail.com>
2021-07-31 00:02:47 +0600

    password encryption completed

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+
 const successMessage = document.querySelector(".successMessage");
+
 var userCounter = 0;
 function registration(jewel){
     userCounter++;
@@ -7,11 +9,13 @@ function registration(jewel){
     let femail = 'email'+userCounter;
     let fpassword = 'password'+userCounter;
     let fcheckbox = 'checkbox'+userCounter;
-
+    //encrypting password
+    let encryptPassword = CryptoJS.AES.encrypt(jewel.password.value, "Secret Passphrase");
     localStorage.setItem(fusername,jewel.firstname.value);
     localStorage.setItem(flusername,jewel.lastname.value);
     localStorage.setItem(femail,jewel.emailaddress.value);
-    localStorage.setItem(fpassword,jewel.password.value);
+    localStorage.setItem(fpassword,encryptPassword);//stored the encrypted password now decrypt the stored password in plain text.
+
     localStorage.setItem(fcheckbox,jewel.checkbox.value);
     
     successMessage.innerHTML = `<h1>Registration successful <a href = "login.html">Please login</a></h1>`;
@@ -27,13 +31,44 @@ function registration(jewel){
     jewel.reset();
     
 }
+//Testing to decrypt the password in javascript.
+
+//console.log(CryptoJS.AES.decrypt(localStorage.password1, "Secret Passphrase").toString(CryptoJS.enc.Utf8));
+
 //Code for login system in javascript.
 function userlogin(form) {
-    if(form.email.value === localStorage.email2 && form.password.value === localStorage.password2){
-        window.open('dashboard.html');
+    if(form.email.value === localStorage.email1 && form.password.value === CryptoJS.AES.decrypt(localStorage.password1, "Secret Passphrase").toString(CryptoJS.enc.Utf8)){
+        window.open('userData/user1/dashboard.html');
+    }
+    else if(form.email.value === localStorage.email2 && form.password.value === CryptoJS.AES.decrypt(localStorage.password2, "Secret Passphrase").toString(CryptoJS.enc.Utf8)){
+        window.open('userData/user2/dashboard.html');
+    }
+    else if(form.email.value === localStorage.email3 && form.password.value === CryptoJS.AES.decrypt(localStorage.password3, "Secret Passphrase").toString(CryptoJS.enc.Utf8)){
+        window.open('userData/user3/dashboard.html');
+    }
+    else if(form.email.value === localStorage.email4 && form.password.value === CryptoJS.AES.decrypt(localStorage.password4, "Secret Passphrase").toString(CryptoJS.enc.Utf8)){
+        window.open('userData/user4/dashboard.html');
+    }
+    else if(form.email.value === localStorage.email5 && form.password.value === CryptoJS.AES.decrypt(localStorage.password5, "Secret Passphrase").toString(CryptoJS.enc.Utf8)){
+        window.open('userData/user5/dashboard.html');
+    }
+    else if(form.email.value === localStorage.email6 && form.password.value === CryptoJS.AES.decrypt(localStorage.password6, "Secret Passphrase").toString(CryptoJS.enc.Utf8)){
+        window.open('userData/user6/dashboard.html');
+    }
+    else if(form.email.value === localStorage.email7 && form.password.value === CryptoJS.AES.decrypt(localStorage.password7, "Secret Passphrase").toString(CryptoJS.enc.Utf8)){
+        window.open('userData/user7/dashboard.html');
+    }
+    else if(form.email.value === localStorage.email8 && form.password.value === CryptoJS.AES.decrypt(localStorage.password8, "Secret Passphrase").toString(CryptoJS.enc.Utf8)){
+        window.open('userData/user8/dashboard.html');
+    }
+    else if(form.email.value === localStorage.email9 && form.password.value === CryptoJS.AES.decrypt(localStorage.password9, "Secret Passphrase").toString(CryptoJS.enc.Utf8)){
+        window.open('userData/user9/dashboard.html');
+    }
+    else if(form.email.value === localStorage.email10 && form.password.value === CryptoJS.AES.decrypt(localStorage.password10, "Secret Passphrase").toString(CryptoJS.enc.Utf8)){
+        window.open('userData/user9/dashboard.html');
     }
     else{
-        alert("Wrong combination");
+        alert("Incorrect your email or password");
     }
 }
 //end the login system code.
@@ -48,5 +83,4 @@ function todoList(form) {
     window.localStorage.setItem(value,listValue);
     alert('Data sent to your Dashboard');
     form.reset();
-
 }
